ec8806063166b4a70fc6b42d8e64892c8bbbecc5
halomademeapc <opr9029@gmail.com>
2019-01-13 14:58:44 -0500

    Style the join game page

--- ClientApp/src/app/pages/pages.module.ts
+++ ClientApp/src/app/pages/pages.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IngameComponent } from './ingame/ingame.component';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
 import { LandingComponent } from './landing/landing.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [IngameComponent, LandingComponent],
   imports: [
     CommonModule,
-    SharedComponentsModule
+    SharedComponentsModule,
+    RouterModule
   ], exports: [
     IngameComponent
   ]
