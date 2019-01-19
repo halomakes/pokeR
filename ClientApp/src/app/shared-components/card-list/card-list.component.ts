3246b47379fe1e3c6839d894fa58cc8356262c44
halomademeapc <opr9029@gmail.com>
2019-01-18 22:13:09 -0500

    disable controls when round ends

--- ClientApp/src/app/shared-components/card-list/card-list.component.ts
+++ ClientApp/src/app/shared-components/card-list/card-list.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input, SimpleChanges, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, Output, EventEmitter, OnChanges, OnInit } from '@angular/core';
 import { Card } from 'src/app/models/entities/card';
 import { PokerService } from 'src/app/services/poker.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.scss']
 })
-export class CardListComponent implements OnChanges {
+export class CardListComponent implements OnChanges, OnInit {
   @Input() cards: Array<Card>;
-  @Input() isEnabled: boolean;
   @Output() cardSelected: EventEmitter<number> = new EventEmitter<number>();
+  isEnabled = true;
   selectedCardId: number = null;
 
   constructor(private service: PokerService) { }
@@ -21,8 +23,19 @@ export class CardListComponent implements OnChanges {
     }
   }
 
+  ngOnInit(): void {
+    this.watchRoundEnd().subscribe();
+    this.watchRoundStart().subscribe();
+  }
+
   onCardSelected = (cardId: number): void => {
     this.service.playCard(cardId).subscribe();
     this.selectedCardId = cardId;
   }
+
+  watchRoundEnd = (): Observable<boolean> =>
+    this.service.roundEnds.pipe(map(() => this.isEnabled = false))
+
+  watchRoundStart = (): Observable<boolean> =>
+    this.service.roundStarts.pipe(map(() => this.isEnabled = true))
 }
