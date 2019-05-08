f7aab449e1aa56675c4663d292b4d11635c4f272
halomademeapc <alex@halomademeapc.com>
2019-05-08 18:43:06 -0400

    start creating new room form

--- PokeR.UWP/MainPage.xaml.cs
+++ PokeR.UWP/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PokeR.Core.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,6 +23,8 @@ namespace PokeR.UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private JoinRoomRequest Request = new JoinRoomRequest();
+
         public MainPage()
         {
             this.InitializeComponent();
